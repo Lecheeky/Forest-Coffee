5b00ec09c93d849c29f97fdf0961333286b426a8
Taha <thadjlakehal@gmail.com>
2024-09-15 17:15:09 +0100

    adding menu files and code similar to index files

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// code for navbar button
+
 const navbarButton = document.querySelector(".navbar-links-button")
 const buttonMenu = document.querySelector(".button-menu")
 const hideButtonMenu = document.querySelector(".button-menu-hide")
@@ -13,3 +15,22 @@ navbarButton.addEventListener("click", function(){
         buttonMenu.classList.add("button-menu-hide");
     }
 })
+
+// code for hero button ("VIEW OUR MENU")
+
+const heroButton = document.querySelector(".hero-button")
+
+heroButton.addEventListener("click", function(){
+    window.location.href = "assets/html/menu.html"
+})
+
+//code for menu div
+
+const menuCard = document.querySelector("#link-card2")
+
+menuCard.addEventListener("click",function(){
+        window.location.href = "assets/html/menu.html"
+    })
+
+
+
